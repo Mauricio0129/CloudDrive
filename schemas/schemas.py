e89796bcce1ae86e4741862a623af58e3931348e
Mauricio0129 <mauricioantonio1227@gmail.com>
2025-09-11 16:23:49 -0400

    Add initial user pydantic model

---
+++ schemas/schemas.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel, Field, EmailStr, SecretStr
+
+class RegisterUser(BaseModel):
+    username: str = Field(min_length=3, max_length=15)
+    email: EmailStr = Field(min_length=3, max_length=35)
+    password: SecretStr = Field(min_length=3, max_length=20)
