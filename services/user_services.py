52221ca609a55e353a1f6e902cb5f169ff1b4fbd
Mauricio0129 <mauricioantonio1227@gmail.com>
2025-09-22 13:23:38 -0400

    fix: update UserServices type hints and improve exception handling logic

--- services/user_services.py
+++ services/user_services.py
@@ -7,16 +7,15 @@ class UserServices:
     def __init__(self, db):
         self.db = db
 
-
 ##At registration we must check neither the username , email are in use
-    async def check_if_user_exist_registration(self, username, email) -> bool:
+    async def check_if_user_exist_registration(self, username, email) -> None:
         async with self.db.acquire() as conn:
             row = await conn.fetchrow("SELECT * FROM users WHERE username = $1 OR email = $2", username, email)
             if row:
                 raise HTTPException(status_code=409, detail="User already exists")
 
 ##Here we use the identifier for both fields as the user could use either email or username to login
-    async def get_user(self, identifier) -> None | UserInDB:
+    async def get_user(self, identifier) -> UserInDB:
         async with self.db.acquire() as conn:
             row = await conn.fetchrow("SELECT * FROM users WHERE username = $1 OR email = $1", identifier)
             if not row:
@@ -25,9 +24,11 @@ class UserServices:
             user_data["id"] = str(user_data["id"])  ##we convert the uuid object to a string
             return UserInDB(**user_data)
 
-    async def register_user(self, username, email,  hashed_password) -> bool:
+    async def register_user(self, username, email,  hashed_password) -> str:
         async with self.db.acquire() as conn:
             row = await conn.fetchrow("INSERT INTO users (username, email, password) "
                                       "VALUES ($1, $2, $3) RETURNING id",
                                       username, email, hashed_password)
-            return bool(row)
+            if not row:
+                raise HTTPException(status_code=500, detail="Failed to register user")
+            return str(row["id"])
